b6b37b5889de61643d25bb18996281be28d48b99
douniahm <dounia.hm@hotmail.com>
2020-03-18 23:43:30 +0000

    setup selenium test

---
+++ src/test/java/JenkinsCICD/TestNG.java
@@ -0,0 +1,27 @@
+package JenkinsCICD;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class TestNG {
+	private WebDriver driver;
+	@Test				
+	public void testEasy() {	
+		driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+	}	
+	@BeforeTest
+	public void beforeTest() {	
+		System.setProperty("webdriver.chrome.driver", "C:/Tools/chromedriver.exe");
+	    driver = new ChromeDriver();  
+	}		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}
+}
